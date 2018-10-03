20675d23bf172abd08688b1adbca7ac78f547e50
Dina <dcaraballo26@gmail.com>
2018-10-03 14:39:25 -0600

    Install eslint and refactor code to clean

--- src/helper.js
+++ src/helper.js
@@ -1,28 +1,27 @@
 class DistrictRepository {
   constructor(data) {
-
-    const filteredDistricts = data.reduce((filteredDistricts, stat) => {
-      if(!filteredDistricts.includes(stat.Location)) {
-        filteredDistricts.push(stat.Location)
+    const filteredDist = data.reduce((filteredDistricts, stat) => {
+      if (!filteredDistricts.includes(stat.Location)) {
+        filteredDistricts.push(stat.Location);
       }
 
-      return filteredDistricts
-    }, [])
+      return filteredDistricts;
+    }, []);
 
-    const cardData = filteredDistricts.map((districtName) => {
-      let cardData = {};
-      cardData[districtName] = {}
+    const cardStats = filteredDist.map((districtName) => {
+      const cardData = {};
+      cardData[districtName] = {};
       data.forEach((stat) => {
-        if(stat.Location === districtName) {
-          cardData[districtName][stat.TimeFrame] = stat.Data
+        if (stat.Location === districtName) {
+          cardData[districtName][stat.TimeFrame] = stat.Data;
         }
-      })
+      });
 
-      return cardData
-    })
+      return cardData;
+    });
 
-    this.stats = cardData;
-  }  
+    this.stats = cardStats;
+  }
 }
 
 
