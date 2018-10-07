642e8b629f86be5155bc8ab183ba1b4bbe2a5dd0
Dina <dcaraballo26@gmail.com>
2018-10-07 14:13:08 -0600

    Add grid styling to CardContainer component

--- src/CardContainer.js
+++ src/CardContainer.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import StatCard from './StatCard';
+import './CardContainer.css'
 
 const CardContainer = (districtData) => {
   // const districtName
@@ -9,13 +10,90 @@ const CardContainer = (districtData) => {
   // const cardId
 
   return(
-    <div>
+    <div className="card-container">
       <h2>District</h2>
-      <StatCard 
-        // districtName={districtName} 
-        // districtStats={districtStats}
-        // id={cardId}
-      />
+      <div className="card-wrapper">
+        <StatCard 
+          // districtName={districtName} 
+          // districtStats={districtStats}
+          // id={cardId}
+        />
+        <StatCard 
+          // districtName={districtName} 
+          // districtStats={districtStats}
+          // id={cardId}
+        />
+        <StatCard 
+          // districtName={districtName} 
+          // districtStats={districtStats}
+          // id={cardId}
+        />
+        <StatCard 
+          // districtName={districtName} 
+          // districtStats={districtStats}
+          // id={cardId}
+        />
+        <StatCard 
+          // districtName={districtName} 
+          // districtStats={districtStats}
+          // id={cardId}
+        />
+        <StatCard 
+          // districtName={districtName} 
+          // districtStats={districtStats}
+          // id={cardId}
+        />
+        <StatCard 
+          // districtName={districtName} 
+          // districtStats={districtStats}
+          // id={cardId}
+        />
+        <StatCard 
+          // districtName={districtName} 
+          // districtStats={districtStats}
+          // id={cardId}
+        />
+        <StatCard 
+          // districtName={districtName} 
+          // districtStats={districtStats}
+          // id={cardId}
+        />
+        <StatCard 
+          // districtName={districtName} 
+          // districtStats={districtStats}
+          // id={cardId}
+        />
+        <StatCard 
+          // districtName={districtName} 
+          // districtStats={districtStats}
+          // id={cardId}
+        />
+        <StatCard 
+          // districtName={districtName} 
+          // districtStats={districtStats}
+          // id={cardId}
+        />
+        <StatCard 
+          // districtName={districtName} 
+          // districtStats={districtStats}
+          // id={cardId}
+        />
+        <StatCard 
+          // districtName={districtName} 
+          // districtStats={districtStats}
+          // id={cardId}
+        />
+        <StatCard 
+          // districtName={districtName} 
+          // districtStats={districtStats}
+          // id={cardId}
+        />
+        <StatCard 
+          // districtName={districtName} 
+          // districtStats={districtStats}
+          // id={cardId}
+        />
+      </div>
     </div>
   )
 }
