2ee41acef81aaa5fe817a3d06c5d46fbc70200df
Dina <dcaraballo26@gmail.com>
2018-10-06 21:54:41 -0600

    Revert "Add tests for App component and add CardContainer component"
    This reverts commit 196a649b383812df1cb346f9481e6bb03b626f08.

--- src/App.js
+++ src/App.js
@@ -1,38 +1,18 @@
 import React, { Component } from 'react';
-import CardContainer from './CardContainer';
 
 import './App.css';
-import data from './data/kindergartners_in_full_day_program';
-import DistrictRepository from './helper';
+import {data} from './data/kindergartners_in_full_day_program';
 
 class App extends Component {
   constructor(data) {
     super(data)
     // const { Location, TimeFrame, Data } = data
-    this.state = {
-      districtData: []
-    }
-  }
-
-  componentDidMount() {
-    this.returnCleanData()
-  }
-
-  returnCleanData() {
-    const districtStats = new DistrictRepository(data);
-    console.log(districtStats)
-    this.setState({
-      districtData: districtStats.stats
-    })
+    this.state = {}
   }
 
   render() {
     return (
-      <div>
-        <h1>Welcome To Headcount 2.0</h1>
-        <CardContainer districtData={this.state.districtData}/>
-      </div>
-
+      <div>Welcome To Headcount 2.0</div>
     );
   }
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,20 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { shallow } from 'enzyme';
 
-describe('App component', () =>  {
-
-  it('should render the card container', () => {
-    const wrapper = shallow(<App />);
-    expect(wrapper).toMatchSnapshot();
-  })
-
-  it('should have a default state length of 181', () => {
-    const wrapper = shallow(<App />);
-    expect(wrapper.state('districtData').length).toEqual(181)
-  })
-
-  // it('should pa')
-
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  // ReactDOM.render(<App />, div);
 });
