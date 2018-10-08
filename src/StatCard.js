3b75fcf9a62df3acb31fc598a476933630592957
Dina <dcaraballo26@gmail.com>
2018-10-07 21:19:03 -0600

    Refactor StatCard.js file with eslint

--- src/StatCard.js
+++ src/StatCard.js
@@ -1,29 +1,41 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import './StatCard.css'
+import './StatCard.css';
 
-const StatCard = ({ districtName, districtYearData}) => {
-  //if li value > 0.5 className="greaterThan"
-  //if li value < 0.5 className="lessThan"
-
+const StatCard = ({ districtName, districtYearData }) => {
   const districtStats = Object.keys(districtYearData).map((year) => {
-
-    if(districtYearData[year] > 0.5) {
-      return <li className="greaterThan">{year}: {districtYearData[year]}</li>
-    } else {
-      return <li className="lessThan">{year}: {districtYearData[year]}</li>
+    if (districtYearData[year] > 0.5) {
+      return (
+        <li className="greaterThan">
+          {year}
+          :
+          {districtYearData[year]}
+        </li>
+      );
     }
-  })
+    return (
+      <li className="lessThan">
+        {year}
+        :
+        {districtYearData[year]}
+      </li>
+    );
+  });
 
-  return(
+  return (
     <div className="stat-card">
       <h2 className="district-name">{districtName}</h2>
       <ul>
         {districtStats}
       </ul>
     </div>
-  )
-}
+  );
+};
+
+StatCard.propTypes = {
+  districtName: PropTypes.string.isRequired,
+  districtYearData: PropTypes.shape({}).isRequired,
+};
 
 export default StatCard;
