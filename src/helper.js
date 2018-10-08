21677cd7e4c2948601a3190129e6b51853baf0aa
Dina <dcaraballo26@gmail.com>
2018-10-08 08:53:34 -0600

    Pass second and third tests for iteration-4

--- src/helper.js
+++ src/helper.js
@@ -49,7 +49,7 @@ class DistrictRepository {
     return { stats: foundMatches };
   }
 
-  findAverage(districtName, dataArray) {
+  findAverage(districtName) {
     const districtSum = Object.values(this.findByName(districtName).stats).reduce((distSum, value) => {
       distSum += Math.round(value * 1000) / 1000;
 
@@ -58,6 +58,21 @@ class DistrictRepository {
     const districtAvg = districtSum/Object.values(this.findByName(districtName).stats).length
 
     return Math.round(districtAvg * 1000) / 1000;
+  }
+
+  compareDistrictAverages(district1, district2) {
+    const dist1Name = district1.toUpperCase()
+    const dist2Name = district2.toUpperCase()
+    const dist1Avg = this.findAverage(district1)
+    const dist2Avg = this.findAverage(district2)
+    const comparisonValue = Math.round((dist1Avg/dist2Avg)*1000) / 1000
+
+    const comparison = {}
+    comparison[dist1Name] = dist1Avg
+    comparison[dist2Name] = dist2Avg
+    comparison.compared = comparisonValue
+
+    return comparison
   }
 }
 
--- src/test/unit/iteration-4.test.js
+++ src/test/unit/iteration-4.test.js
@@ -8,12 +8,12 @@ describe('DistrictRepository iteration 0', () =>  {
     expect(district.findAverage('ACADEMY 20')).toBe(.407)
   });
 
-  xtest('compareDistrictAverages ACADEMY 20 against YUMA SCHOOL DISTRICT 1', () => {
+  test('compareDistrictAverages ACADEMY 20 against YUMA SCHOOL DISTRICT 1', () => {
     const result =  { "ACADEMY 20": 0.407, "YUMA SCHOOL DISTRICT 1": 0.909, "compared": 0.448 }
     expect(district.compareDistrictAverages('ACADEMY 20', 'YUMA SCHOOL DISTRICT 1')).toEqual(result);
   });
 
-  xtest('compareDistrictAverages is case insensitive', () => {
+  test('compareDistrictAverages is case insensitive', () => {
     const result =  { "ACADEMY 20": 0.407, "YUMA SCHOOL DISTRICT 1": 0.909, "compared": 0.448 }
     expect(district.compareDistrictAverages('ACADeMY 20', 'YUMA ScHOoL DiStRICT 1')).toEqual(result);
   });
