189183b27be1d80068e8ce7aefc9c6ac2a7e24bc
Dina <dcaraballo26@gmail.com>
2018-10-07 20:55:41 -0600

    Refactor helper.js file with eslint

--- src/helper.js
+++ src/helper.js
@@ -8,17 +8,17 @@ class DistrictRepository {
       return filteredDistricts;
     }, []);
 
-    this.stats = filteredDist.map(districtName => {
+    this.stats = filteredDist.map((districtName) => {
       const cardData = {};
       cardData.district = districtName;
       cardData.stats = {};
-      data.forEach(stat => {
-        if(typeof stat.Data !== 'number') {
-          stat.Data = 0
+      data.forEach((stat) => {
+        if (typeof stat.Data !== 'number') {
+          stat.Data = 0;
         }
 
         if (stat.Location.toUpperCase() === districtName) {
-          cardData.stats[stat.TimeFrame] = Math.round(stat.Data*1000)/1000;
+          cardData.stats[stat.TimeFrame] = Math.round(stat.Data * 1000) / 1000;
         }
       });
 
@@ -27,26 +27,26 @@ class DistrictRepository {
   }
 
   findByName(districtName) {
-    if(!districtName) {
-      return undefined
+    if (!districtName) {
+      return undefined;
     }
-    const found = this.stats.find(stat => {  
-      return stat.district.toUpperCase() === districtName.toUpperCase()
-    })
+    const found = this.stats.find((stat) => {
+      return stat.district.toUpperCase() === districtName.toUpperCase();
+    });
 
-    return found
+    return found;
   }
 
   findAllMatches(districtName) {
-    if(!districtName) {
-      return this.stats
+    if (!districtName) {
+      return this.stats;
     }
 
     const foundMatch = this.stats.filter((stat) => {
-      return stat.district.includes(districtName.toUpperCase())
-    })
-    
-    return {stats: foundMatch}
+      return stat.district.includes(districtName.toUpperCase());
+    });
+
+    return { stats: foundMatch };
   }
 }
 
